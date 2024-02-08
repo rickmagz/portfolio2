622e0ce5d4f85eeafe18c21f906d143f98aead56
rickmagz <ramnashor2@gmail.com>
2024-02-08 11:46:00 +0800

    Refining depedencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/header/Header';
-import Body from './components/body/Body';
-import Footer from './components/footer/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <Header />
         <Body />
-        <Footer/>
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
