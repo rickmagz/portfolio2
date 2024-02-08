6591729a1439a9bd327ec7000189fb5f65eeacce
rickmagz <ramnashor2@gmail.com>
2024-02-08 11:46:25 +0800

    Modified navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,57 +1,33 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <div className="container px-5">
-      <a className="navbar-brand" href="index.html">
-        <img
-          className="rounded img-fluid"
-          src="src/assets/Picture1.png"
-          width={60}
-          height={60}
-        />
-        <span className="fw-bolder text-secondary" />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-          <li className="nav-item">
-            <a className="nav-link" href="index.html">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="resume.html">
-              Resume
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="projects.html">
-              Projects
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="contact.html">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  )
-}
+    <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            className="rounded img-fluid"
+            src="src/assets/Picture1.png"
+            width={60}
+            height={60}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
